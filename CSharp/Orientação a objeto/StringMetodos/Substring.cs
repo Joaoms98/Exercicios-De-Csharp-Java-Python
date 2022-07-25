449c2e6a147ecb1ca4899df139b93d930e787da0
Joaoms98 <jvm1800@gmail.com>
2022-07-25 14:47:09 -0300

    @Joaoms98

---
+++ CSharp/Orientação a objeto/StringMetodos/Substring.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+static class LogLine
+{
+    public static string Message(string logLine)
+    {
+        int start = logLine.IndexOf(":") + 1;
+        return logLine.Substring(start).Trim();
+        //Assert.Equal("Stack overflow", LogLine.Message("[ERROR]: Stack overflow"));
+    }
+
+    public static string LogLevel(string logLine)
+    {
+        int start = logLine.IndexOf("[") + 1;
+        int end = logLine.IndexOf("]") - 1;
+        return logLine.Substring(start, end).ToLower();
+        //Assert.Equal("error", LogLine.LogLevel("[ERROR]: Disk full"));
+    }
+
+    public static string Reformat(string logLine)
+    {
+        string error = LogLevel(logLine);
+        string message = Message(logLine);
+        return (message + " (" + error + ")");
+        //Assert.Equal("Segmentation fault (error)", LogLine.Reformat("[ERROR]: Segmentation fault"));
+    }
+}
