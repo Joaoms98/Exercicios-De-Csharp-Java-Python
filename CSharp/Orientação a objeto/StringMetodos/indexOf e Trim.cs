6c1061eb943d2de31300c165d5b8940304f3f0a3
Joaoms98 <jvm1800@gmail.com>
2022-07-25 13:38:03 -0300

    @Joaoms98

---
+++ CSharp/Orientação a objeto/StringMetodos/indexOf e Trim.cs
@@ -0,0 +1,26 @@
+﻿namespace ex
+{
+    class Program
+    {
+
+        class msg
+        {
+            //indexof seleciona o numero da letra na string que você determinou entre parenteses e o remove exclui o restante da string determinando a pos, o trim elimina um caractere após o termino do indexof
+            public void Hello()
+            {
+                string hello = "[Hello]HHoão";
+                int pos = hello.IndexOf("]")+2;
+                hello = hello.Remove(pos).Trim('H');
+                Console.WriteLine(hello);
+            }
+
+            //retorno: 
+        }
+
+        static void Main(string[]args)
+        {
+            msg msg = new msg();
+            msg.Hello();
+        }
+    } 
+}
