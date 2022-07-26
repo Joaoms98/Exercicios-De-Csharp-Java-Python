c8b4d7017a3c3623678b49d6c13b36c11e3c574a
Joaoms98 <jvm1800@gmail.com>
2022-07-26 11:37:55 -0300

    @Joaoms98

---
+++ CSharp/Orientação a objeto/StringMetodos/Conceito this(Extensão de métodos)/ConsoleApp1/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public static class Class1
+    {
+        public static string SubstringAfter(this String str, string delimiter)
+        {
+            return str.Substring(str.IndexOf(delimiter) + delimiter.Length);
+        }
+
+        public static string SubstringBetween(this string str,string startdel,string enddel)
+        {
+            int indexstart = str.IndexOf(startdel);
+            int indexend = str.IndexOf(enddel);
+            return str.Substring(indexstart + 1, indexend - 1).Trim() ;
+        }
+
+        public static string Message(this string str)
+        {
+            int indexstart = str.IndexOf(":") +1;
+            return str.Substring(indexstart).Trim();
+        }
+
+        public static string LogLevel(this string str)
+        {
+            return str.SubstringBetween("[", "]");
+        }
+    }
+}
---
+++ CSharp/Orientação a objeto/StringMetodos/Conceito this(Extensão de métodos)/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!".SubstringAfter(" "));
+            //=> return World!
+
+            Console.WriteLine("SOMETHING", "FIND >>> SOMETHING <===< HERE".SubstringBetween(">>> ", " <===<"));
+            //=> return SOMETHING
+
+            Console.WriteLine("[ERROR]: Missing ; on line 20.".Message());
+            //=> return Missing ; on line 20.
+
+            Console.WriteLine("[WARNING]: Library is deprecated.".LogLevel());
+            //=> return WARNING
+        }
+    }
+}
