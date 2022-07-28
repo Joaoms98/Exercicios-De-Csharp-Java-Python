e388fc33f3fcbcf0acb54be3fdf3fa0bd94395c2
Joaoms98 <jvm1800@gmail.com>
2022-07-28 17:10:15 -0300

    @

---
+++ CSharp/Orientação a objeto/SistemaBancarioSimples/ContaCorrente.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conta
+{
+    public  class ContaCorrente
+    {
+        public double Saldo;
+        public string? Status;
+        public string Transacao(double saque)
+        {
+            if (Saldo < saque)
+                return Status= "Saldo insuficiente";
+            else
+                Saldo = Saldo-saque;
+                return Status = "Operação Realizada com sucesso!";
+        }
+    }
+}
---
+++ CSharp/Orientação a objeto/SistemaBancarioSimples/Program.cs
@@ -0,0 +1,22 @@
+﻿using Conta;
+
+
+ContaCorrente conta1 = new ContaCorrente();
+conta1.Saldo = 1000;
+
+string Esc;
+
+do
+{
+    Console.WriteLine($"Saldo Atual "+conta1.Saldo);
+    Console.Write($"Informe a quantinha de saque:");
+
+    conta1.Transacao(Convert.ToDouble(Console.ReadLine()));
+    Console.WriteLine(conta1.Status);
+    Console.WriteLine($"Saldo Atual " + conta1.Saldo);
+
+    Console.WriteLine("############# Aperte qualquer tecla para sair ou digite 1 para realizar uma nova transação ##############");
+    Esc = Console.ReadLine();
+} while (Esc == "1");
+
+
