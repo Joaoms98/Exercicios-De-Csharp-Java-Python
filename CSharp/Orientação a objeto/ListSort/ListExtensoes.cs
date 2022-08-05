df1c380e04898bde9749c55c0cb4c91d72c6f215
Joaoms98 <jvm1800@gmail.com>
2022-08-05 17:41:33 -0300

    @

---
+++ CSharp/Orientação a objeto/ListSort/ListExtensoes.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListExtensoes.Extensoes
+{
+    public static class ListExtensoes
+    {
+        public static void Addmany<T>(this List<T> lista, params T[] itens)
+        {
+            foreach (T item in itens)  //percorrer a lista
+            {
+                lista.Add(item);
+            }
+        }   
+
+        static void Teste()     
+        {
+            List<int> idades = new List<int>();
+            idades.Add(1); 
+            idades.Add(2);
+            idades.Add(3);
+            idades.Add(4);
+
+            idades.Addmany(1, 2, 3, 4, 5, 6, 7, 8, 98, 64); //método para adicionar vários
+        }
+    }
+}
+    
---
+++ CSharp/Orientação a objeto/ListSort/Sort.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleApp1
+{
+    public class Sort
+    {
+       static void Main(string[] args)
+       {
+            var nomes = new List<string>()
+            {
+                "Adriano",
+                "Felipe francisco",
+                "Saci-perere",
+                "Ana"
+            };
+
+            nomes.Sort();
+
+            foreach (var item in nomes)
+            {
+                Console.WriteLine(item);
+                //return nomes em ordem alfabética
+            }
+       }
+    }
+}
