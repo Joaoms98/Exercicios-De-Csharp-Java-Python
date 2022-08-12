adf60f51b460fec8c5dfd67a20197b9133288ea5
Joaoms98 <jvm1800@gmail.com>
2022-08-12 17:24:48 -0300

    @

---
+++ CSharp/CQRSeCleanCode/APIGames/APIMet/Controllers/GameController.cs
@@ -0,0 +1,48 @@
+﻿using APIGames.models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace APIMet.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class GameController : ControllerBase
+    {
+        private static List<GameModel> games = new List<GameModel>();
+
+        [HttpPost]
+        public void NewGame([FromBody] GameModel game)
+        {
+            game.Id = Guid.NewGuid();
+            games.Add(game);
+        }
+
+        [HttpGet]
+        public IActionResult ListGames()
+        {
+            try
+            {
+                return Ok(games);
+            }
+            catch
+            {
+                return NoContent();
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult SeachGameforId(Guid id)
+        {
+            try
+            {
+                return Ok(games.FirstOrDefault(g => g.Id == id));
+            }
+            catch(Exception)
+            {
+                return NotFound();
+            }
+        }
+    }
+}
---
+++ CSharp/CQRSeCleanCode/APIGames/APIMet/models/GameModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace APIGames.models
+{
+    public class GameModel
+    {
+        public Guid Id { get; set; }
+        [Required]
+        public string Name { get; set;}
+        public string Description { get; set;}
+        [Required]
+        public double Price { get; set; }
+    }
+}
